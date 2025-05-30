d1d3990c34412d6668ff5d38fd844ff24a331a2e
Adil Rameto <adil.rameto11@outlook.com>
2025-05-30 23:33:44 +0100

    Create t_ff.v

---
+++ sequential_logic_design/4_bit_sync_counter/t_ff.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: adil rameto
+// 
+// Create Date: 30.05.2025 
+// Design Name: 
+// Module Name: t_ff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module t_ff(
+    input clk,
+    input rst,
+    output reg q
+);
+    always @(negedge clk or posedge rst) begin
+        if (rst) 
+            q <= 1'b0;
+        else 
+            q <= ~q;
+    end
+    
+    
+endmodule
